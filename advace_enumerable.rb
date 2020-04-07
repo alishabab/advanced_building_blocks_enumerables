1222a1cd59411e5aaf83c27b63448974a994f021
Shabab Ali <shababsaifi@gmail.com>
2020-04-07 16:25:19 +0530

    added my_select method

--- advace_enumerable.rb
+++ advace_enumerable.rb
@@ -18,6 +18,19 @@ module Enumerable
     end
     self
   end
+  # my_select
+
+  def my_select
+    i = 0
+    my_arr = []
+    while i < size
+      my_arr.push(self[i]) if yield(self[i])
+      i += 1
+    end
+    my_arr
+  end
 end
 [1, 2, 2, 4, 6].my_each { |x| puts x }
 [1, 2, 3, 4, 5].my_each_with_index { |e, i| puts "element:#{e}, index:#{i}" }
+res = [10, 20, 9, 8, 30, 40].my_select { |x| x > 10 }
+puts res
