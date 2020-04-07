ea66df4bfe8631130d2b158767cc0f11ed69b37c
Shabab Ali <shababsaifi@gmail.com>
2020-04-07 16:09:24 +0530

    Added my_each_with_index method

--- advace_enumerable.rb
+++ advace_enumerable.rb
@@ -8,6 +8,16 @@ module Enumerable
     end
     self
   end
-end
+  # my_each_with_index
 
+  def my_each_with_index
+    i = 0
+    while i < size
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
+end
 [1, 2, 2, 4, 6].my_each { |x| puts x }
+[1, 2, 3, 4, 5].my_each_with_index { |e, i| puts "element:#{e}, index:#{i}" }
