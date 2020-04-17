b46d75155dd7dbf5c59311be688b35d323ca38f1
Shabab Ali <shababsaifi@gmail.com>
2020-04-17 14:03:44 +0530

    Added test for my_each

--- spec/advance_enumerable_spec.rb
+++ spec/advance_enumerable_spec.rb
@@ -1,1 +1,10 @@
 require '../advace_enumerable.rb'
+RSpec.describe Enumerable do
+  include Enumerable
+  let(:arr) { [2, 1, 7] }
+  describe '#my_each' do
+    it 'performs operation on each element of array if block is given' do
+      expect(arr.my_each { |x| puts x }).to eql(arr.each { |x| puts x })
+    end
+  end
+end
