c8d22500a13540bbfdd56ee203b68daf0fbc7dae
Murilo Roque <muriloengqui@gmail.com>
2020-04-17 06:03:39 -0300

    Created tests for the first three methods

--- spec/advance_enumerable_spec.rb
+++ spec/advance_enumerable_spec.rb
@@ -6,5 +6,44 @@ RSpec.describe Enumerable do
     it 'performs operation on each element of array if block is given' do
       expect(arr.my_each { |x| puts x }).to eql(arr.each { |x| puts x })
     end
+    it 'returns to_enum if no block is given' do
+      expect(arr.my_each).to be_a(Enumerator)
+    end
+    it 'returns an error if an argument is given' do
+      expect { arr.my_each(1) }.to raise_error(ArgumentError)
+    end
+    it 'returns an error if something else is given instead of an array' do
+      expect { 1.my_each }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'performs operation on each element of array if block is given' do
+      expect(arr.my_each_with_index { |x, n| puts x, n }).to eql(arr.each_with_index { |x, n| puts x, n })
+    end
+    it 'returns to_enum if no block is given' do
+      expect(arr.my_each_with_index).to be_a(Enumerator)
+    end
+    it 'returns an error if an argument is given' do
+      expect { arr.my_each_with_index(1) }.to raise_error(ArgumentError)
+    end
+    it 'returns an error if something else is given instead of an array' do
+      expect { 1.my_each_with_index }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe '#my_each' do
+    it 'performs operation on each element of array if block is given' do
+      expect(arr.my_select { |x| x < 1 }).to eql(arr.select { |x| x < 1 })
+    end
+    it 'returns to_enum if no block is given' do
+      expect(arr.my_select).to be_a(Enumerator)
+    end
+    it 'returns an error if an argument is given' do
+      expect { arr.my_select(1) }.to raise_error(ArgumentError)
+    end
+    it 'returns an error if something else is given instead of an array' do
+      expect { 1.my_select }.to raise_error(NoMethodError)
+    end
   end
 end
