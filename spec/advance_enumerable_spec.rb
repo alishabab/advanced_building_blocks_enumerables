5f8a169e2b50a5ce494f557ac42407816ebcd4fa
Shabab Ali <shababsaifi@gmail.com>
2020-04-17 14:50:49 +0530

    Added test for my_all? method

--- spec/advance_enumerable_spec.rb
+++ spec/advance_enumerable_spec.rb
@@ -32,18 +32,15 @@ RSpec.describe Enumerable do
     end
   end
 
-  describe '#my_each' do
-    it 'performs operation on each element of array if block is given' do
-      expect(arr.my_select { |x| x < 1 }).to eql(arr.select { |x| x < 1 })
+  describe '#my_all?' do
+    it 'returns true if all the items of array satisfies the condition' do
+      expect(arr.my_all? { |x| x < 1 }).to eql(arr.all? { |x| x < 1 })
     end
-    it 'returns to_enum if no block is given' do
-      expect(arr.my_select).to be_a(Enumerator)
+    it 'returns true if pattern is matched or array has all elements same as argument' do
+      expect(arr.my_all?(/d/)).to eql(arr.all?(/d/))
     end
-    it 'returns an error if an argument is given' do
-      expect { arr.my_select(1) }.to raise_error(ArgumentError)
-    end
-    it 'returns an error if something else is given instead of an array' do
-      expect { 1.my_select }.to raise_error(NoMethodError)
+    it 'returns true if all the elements are true if we dont pass block and argument' do
+      expect(arr.my_all?).to be true
     end
   end
 end
